da29df2cfdaaf42837997deed878eb3eef512fab
Olivier Van Aken <olivier.va@outlook.com>
2024-02-12 22:15:27 +0100

    implemented mains

---
+++ cmd/nuke/main.go
@@ -0,0 +1,8 @@
+package main
+
+import "github.com/restechnica/nuke/pkg/cli/exec"
+
+// main bootstraps the `nuke` CLI.
+func main() {
+	_ = exec.Run()
+}
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"github.com/restechnica/nuke/pkg/cli/exec"
+)
+
+// main bootstraps the `sbot` CLI app.
+func main() {
+	_ = exec.Run()
+}
